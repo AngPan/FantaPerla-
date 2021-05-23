cded7a9dbc425d74e0d08feb3578b78c6fe8169e
AngPan <angelopanniello93@gmail.com>
2021-05-23 02:36:40 +0200

    upd

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const $ = (...s) => {
+    const str = Array.isArray(s[0]) ? String.raw(...s) : s[0];
+    return str[0] === '<' ? document.createRange().createContextualFragment(str) : document.querySelector(str);
+}
+
+const $$ = (...s) => {
+    const str = Array.isArray(s[0]) ? String.raw(...s) : s[0];
+    return Array.from(str[0] === '<' ? $(str).children : document.querySelectorAll(str));
+}
+
 /* Function for fetching a document fragment and replacing an element with it */
 function replace(el, url) {
     return fetch(url)
