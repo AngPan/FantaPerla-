b7f5fff33bcadea5c2d1d081aabdca0540184f93
AngPan <angelopanniello93@gmail.com>
2021-05-30 15:55:45 +0200

    upd

--- index.js
+++ index.js
@@ -17,3 +17,42 @@ function replace(el, url) {
             el.remove();
         });
 }
+
+const iO = new IntersectionObserver(entries => entries.forEach(entry => {
+    const hash = '#' + entry.target.id;
+    const navEl = $ `[href="${hash}"]`;
+    if (entry.isIntersecting && entry.intersectionRatio > 0.5) {
+        navEl.classList.add('focus');
+        updateHistory(hash);
+    } else {
+        navEl.classList.remove('focus');
+    }
+}), {
+    root: $ `main`,
+    threshold: 0.5
+});
+
+
+
+window.addEventListener('DOMContentLoaded', () => $$ `article`.map(a => iO.observe(a)));
+window.addEventListener('hashchange', function(e) {
+    const articleToShow = $(window.location.hash || '#' + $ `article`.id);
+    articleToShow.scrollIntoView();
+    e.preventDefault();
+}, false);
+
+/* Update the window URL on swipe, this is throttled so that the history doesn't get filled with useless entries*/
+function updateHistory(hash) {
+    clearTimeout(updateHistory.timeout);
+    updateHistory.timeout = setTimeout(function() {
+        if (window.location.hash !== hash) {
+            if (location.hash !== '') {
+                history.pushState({}, window.title, hash);
+            } else {
+
+                // On first page load update the URL in place
+                history.replaceState({}, window.title, hash);
+            }
+        }
+    }, 1000);
+}
