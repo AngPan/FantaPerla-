7cf4ca7296059d2b6ce1ddc97fa24667762b0cab
AngPan <angelopanniello93@gmail.com>
2021-07-26 20:35:33 +0200

    upd

--- index.js
+++ index.js
@@ -56,3 +56,59 @@ function updateHistory(hash) {
         }
     }, 1000);
 }
+
+window.addEventListener('load', e => {
+    // new PWAConfApp();
+    registerSW();
+});
+
+async function registerSW() {
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('./sw.js');
+        } catch (e) {
+            console.log("ServiceWorker registration failed.");
+        }
+    } else {
+        document.querySelector('.alert').removeAttribute('hidden');
+    }
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+    // not show the default browser install app prompt
+    event.preventDefault();
+    // add the banner here or make it visible
+    // …
+    // console.log("App non installata");
+    // $('#install_button').css('display', 'flex');
+
+    // save the event to use it later
+    // (it has the important prompt method and userChoice property)
+    window.promptEvent = event;
+});
+
+
+document.addEventListener('click', function(event) {
+    if (event.target.matches('.install-trigger')) {
+        addToHomeScreen();
+    }
+});
+
+function addToHomeScreen() {
+    // show the install app prompt
+    window.promptEvent.prompt();
+
+    // handle the Decline/Accept choice of the user
+    window.promptEvent.userChoice.then(function(choiceResult) {
+        // hide the prompt banner here
+        // …
+
+        if (choiceResult.outcome === 'accepted') {
+            console.info('mm User accepted the A2HS prompt');
+        } else {
+            console.info('mm User dismissed the A2HS prompt');
+        }
+
+        window.promptEvent = null;
+    });
+}
